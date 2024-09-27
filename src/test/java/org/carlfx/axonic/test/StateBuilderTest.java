425551ed0880bd378a23b1c1cf720914694e6e26
Carl Dea <cardea@deloitte.com>
2024-09-26 20:58:24 -0400

    Initial add

---
+++ src/test/java/org/carlfx/axonic/test/StateBuilderTest.java
@@ -0,0 +1,23 @@
+package org.carlfx.axonic.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@DisplayName("StateBuilder Test")
+public class StateBuilderTest {
+    private static final Logger LOG = LoggerFactory.getLogger(StateBuilderTest.class);
+
+
+    @Test
+    @DisplayName("State Building Test")
+    void stateBuildingTest() {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
